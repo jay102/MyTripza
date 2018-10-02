fb82252976e05d01c00ba43b02570f62c0ef1be7
jay102 <jordunjames@gmail.com>
2018-10-02 01:55:41 +0100

    myBranch first commit

--- app/src/main/java/com/mytripza/SplashActivity.kt
+++ app/src/main/java/com/mytripza/SplashActivity.kt
@@ -1,22 +1,80 @@
 package com.mytripza
 
 
-
+import android.app.Activity
+import android.content.Intent
+import android.graphics.PixelFormat
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.mytripza.controllers.Otp
 
 
-/**
- * A simple [Fragment] subclass.
- */
-class SplashFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_splash, container, false)
-        return view
+class SplashActivity : Activity() {
+
+    fun onAttachedtoWindow() {
+        super.onAttachedToWindow()
+        val window = window
+        window.setFormat(PixelFormat.RGBA_8888)
+    }
+    var splashTread: Thread? = null
+    var imageView: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash)
+        /*    imageView = (ImageView)findViewById(R.id.splash);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        int[] ids = new int[]{R.drawable.logo2,R.drawable.logo2, R.drawable.logo2};
+        Random randomGenerator = new Random();
+        int r= randomGenerator.nextInt(ids.length);
+        this.imageView.setImageDrawable(getResources().getDrawable(ids[r]));*/
+        Animation()
+
     }
 
-}// Required empty public constructor
+
+    internal fun Animation() {
+        var anim = AnimationUtils.loadAnimation(this, R.anim.alpha)
+        anim.reset()
+        val l = findViewById(R.id.lin_lay) as LinearLayout
+        l.clearAnimation()
+        l.startAnimation(anim)
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate)
+        anim.reset()
+        val iv = findViewById(R.id.splash) as ImageView
+        iv.clearAnimation()
+        iv.startAnimation(anim)
+
+        splashTread = object : Thread() {
+            override fun run() {
+                try {
+                    var waited = 0
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        Thread.sleep(100)
+                        waited += 100
+                    }
+                    val intent = Intent(this@SplashActivity,
+                            Otp::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+                    startActivity(intent)
+                    this@SplashActivity.finish()
+                } catch (e: InterruptedException) {
+                    // do nothing
+                } finally {
+                    this@SplashActivity.finish()
+                }
+
+            }
+        }
+        (splashTread as Thread).start()
+    }
+}
---
+++ app/src/main/java/com/mytripza/controllers/Otp.java
@@ -0,0 +1,19 @@
+package com.mytripza.controllers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mytripza.R;
+
+public class Otp extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp_layout);
+
+
+    }
+
+
+}
