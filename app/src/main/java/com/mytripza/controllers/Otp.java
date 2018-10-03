78c2dd16ed0dcf64622bcf36a0f3261d5ff33e8a
jay102 <jordunjames@gmail.com>
2018-10-03 01:42:19 +0100

    added 2nd otp screen and linked buttons

--- app/src/main/java/com/mytripza/controllers/Otp.java
+++ app/src/main/java/com/mytripza/controllers/Otp.java
@@ -1,18 +1,29 @@
 package com.mytripza.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.mytripza.R;
 
 public class Otp extends AppCompatActivity{
+    Button submit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.otp_layout);
 
-
+        submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Otp.this,Otp2.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
